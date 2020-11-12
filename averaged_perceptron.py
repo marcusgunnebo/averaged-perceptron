0d8a1d572a03cf5f1387b63f6499d2d72488372f
Marcus <marcus@marcus.home>
2020-11-12 15:30:39 +0100

    second

--- averaged_perceptron.py
+++ averaged_perceptron.py
@@ -3,7 +3,7 @@ from sklearn.datasets import load_files
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 
-class averagePerceptron: 
+class averagedPerceptron: 
     def __init__(self, iter):
         self.iter =iter
 
@@ -45,17 +45,17 @@ x_train, x_test, y_train, y_test = train_test_split(vector.toarray(), data.targe
 y_test = np.where(y_test==0, -1, y_test)
 y_train = np.where(y_train==0, -1, y_train)
 
-model1 = averagePerceptron(10)
+model1 = averagedPerceptron(10)
 trainedModel = model1.fit(x_train, y_train)
 y_pred1 = model1.predict(x_test,trainedModel)
 print('Accuracy average perceptron 10 iteration: %.5f' % model1.score(y_pred1,y_test))
 
-model2 = averagePerceptron(100)
+model2 = averagedPerceptron(100)
 trainedModel = model2.fit(x_train, y_train)
 y_pred2 = model2.predict(x_test,trainedModel)
 print('Accuracy average perceptron 100 iteration: %.5f' % model1.score(y_pred2,y_test))
 
-model3 = averagePerceptron(1000)
+model3 = averagedPerceptron(1000)
 trainedModel = model3.fit(x_train, y_train)
 y_pred3 = model3.predict(x_test,trainedModel)
 print('Accuracy average perceptron 1000 iteration: %.5f' % model1.score(y_pred3,y_test))
